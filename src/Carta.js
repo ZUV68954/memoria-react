6153965071bcf416944956af93dd6383a392a966
ZUV68954 <ZUV68954@educastur.es>
2024-11-14 15:45:21 +0100

    Las imágenes se dan la vuelta y llevan la cuenta de forma cuestionable.
    Co-authored-by: Diego Fernández Alonso <diegofa85@users.noreply.github.com>

--- src/Carta.js
+++ src/Carta.js
@@ -1,25 +1,28 @@
 import React, { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 
-function Carta({ id, anverso, reverso, volteada }) {
+function Carta({ id, anverso, reverso, comprobar }) {
     // onclick cambia la variable o la ruta de la variable
-    const [cara, setCara] = useState(true);
+    const [volteada, setCara] = useState(false);
     const [nombreImagen, setNombreImagen] = useState(reverso);
 
     const cambio = () => {
-        if (cara) {
-            setNombreImagen(reverso);
-        }
-        else {
+        if (volteada) {
             setNombreImagen(anverso);
         }
-        setCara(!cara);
+        else {
+            setNombreImagen(reverso);
+        }
+        setCara(!volteada);
+        comprobar(id);
     }
 
     // Por alguna razón el primer click no funciona, así que llamo una única vez al click y ahora ya funciona el primer click :).
     useEffect(() => {
         cambio();
     }, []);
+
+
 
     return <img src={nombreImagen} onClick={cambio} alt="" class="border border-primary img-fluid mb-2" id={id}></img>
 }
--- src/Tablero.js
+++ src/Tablero.js
@@ -5,15 +5,29 @@ export default function Tablero() {
 
     // La idea es definir un array de cartas, así les podemos poner distintas imágenes y posiblemente compararlas (espero).
     const [cartas] = useState([
-        { id: 1, anverso: "anverso.webp", reverso: "reverso.webp", volteada: true },
-        { id: 2, anverso: "anverso.webp", reverso: "reverso.webp", volteada: true },
-        { id: 3, anverso: "anverso.webp", reverso: "reverso.webp", volteada: true },
-        { id: 4, anverso: "anverso.webp", reverso: "reverso.webp", volteada: true },
-        { id: 5, anverso: "anverso.webp", reverso: "reverso.webp", volteada: true },
-        { id: 6, anverso: "anverso.webp", reverso: "reverso.webp", volteada: true },
-        { id: 7, anverso: "anverso.webp", reverso: "reverso.webp", volteada: true },
-        { id: 8, anverso: "anverso.webp", reverso: "reverso.webp", volteada: true },
+        { id: 1, anverso: "gallo.webp", reverso: "reverso.webp" },
+        { id: 2, anverso: "vaca.webp", reverso: "reverso.webp" },
+        { id: 3, anverso: "gallo.webp", reverso: "reverso.webp" },
+        { id: 4, anverso: "gallo.webp", reverso: "reverso.webp" },
+        { id: 5, anverso: "vaca.webp", reverso: "reverso.webp" },
+        { id: 6, anverso: "burro.webp", reverso: "reverso.webp" },
+        { id: 7, anverso: "gallo.webp", reverso: "reverso.webp" },
+        { id: 8, anverso: "burro.webp", reverso: "reverso.webp" },
     ]);
+
+    const [volteada, setVolteada] = useState(false);
+    const [totalVolteadas, setTotalVolteadas] = useState(1);
+
+    const comprobar = (id) => {
+        if (volteada) {
+            setVolteada(false);
+            setTotalVolteadas(totalVolteadas + 1);
+        }
+        else {
+            setVolteada(true);
+            setTotalVolteadas(totalVolteadas - 1);
+        }
+    }
 
     return (
         <div class="container pt-5">
@@ -24,11 +38,16 @@ export default function Tablero() {
                             id={carta.id}
                             anverso={carta.anverso}
                             reverso={carta.reverso}
+                            comprobar={comprobar}
                         />
                     </div>
                 ))
                 }
             </div>
+            <div>
+                <p>Son un total de {totalVolteadas}</p>
+            </div>
         </div>
+
     )
 }
